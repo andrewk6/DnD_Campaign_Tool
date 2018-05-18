d9b6941804d000ff9c251919c1b4ddae5c0333c0
andrewk6 <andrew.knox112@gmail.com>
2018-05-18 16:04:46 -0400

    Cleaned up DM_Window

--- src/DM_Window.java
+++ src/DM_Window.java
@@ -21,7 +21,6 @@ public class DM_Window {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello");
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -38,8 +37,6 @@ public class DM_Window {
 	 * Create the application.
 	 */
 	public DM_Window() {
-		System.out.println("Calling Export");
-		export();
 		initialize();
 	}
 
@@ -50,49 +47,6 @@ public class DM_Window {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-	
-	public void export() {
-		System.out.println("Hello Out There");
-		Armor a = new Armor();
-		Coin c = new Coin(Coin.COIN_PLATINUM, 1234);
-		Item i = new Item();
-		MagicItem mI = new MagicItem();
-		Weapon w = new Weapon();
-		
-		
-		File f = new File("itemImport.dat");
-		if (f.exists()) {
-			System.out.println("Reading the Class");
-			try {
-				FileInputStream fin = new FileInputStream(f);
-				ObjectInputStream ois = new ObjectInputStream(fin);
-				a = (Armor) ois.readObject();
-				c = (Coin) ois.readObject();
-				i = (Item) ois.readObject();
-				mI = (MagicItem) ois.readObject();
-				w = (Weapon) ois.readObject();
-				ois.close();
-				//fin.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		} else {
-			System.out.println("Printing the Class");
-			try {
-				FileOutputStream fout = new FileOutputStream(f);
-				ObjectOutputStream oos = new ObjectOutputStream(fout);
-				oos.writeObject(a);
-				oos.writeObject(c);
-				oos.writeObject(i);
-				oos.writeObject(mI);
-				oos.writeObject(w);
-				oos.close();
-				//fout.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
 	}
 
 }
